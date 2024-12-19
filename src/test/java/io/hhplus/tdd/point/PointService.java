1da4c2bc6b1a2447835bf02487e62ce21085fae9
geonduck <geonduck93@gmail.com>
2024-12-19 21:29:55 +0900

    1. PointServiceTest - 테스트 코드를 위한 리펙토링

--- src/test/java/io/hhplus/tdd/point/PointService.java
+++ src/test/java/io/hhplus/tdd/point/PointService.java
@@ -4,6 +4,7 @@ import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -18,12 +19,12 @@ public class PointService {
     }
 
     public UserPoint charge(final Long userId, Long amount) {
-        amount = checkAmount(amount);
+        long addAmount = addAmount(amount);
         UserPoint userPoint = userPointTable.selectById(userId);
-        return updatedPoint(userId, userPoint.point() + amount, TransactionType.CHARGE);
+        return updatedPoint(userId, userPoint.point() + addAmount, TransactionType.CHARGE);
     }
 
-    public long checkAmount(final long amount) {
+    public long addAmount(final long amount) {
         if (amount < 10_000) {
             throw new IllegalStateException("최소 10_000원 이상 충전 가능합니다");
         }
@@ -37,22 +38,25 @@ public class PointService {
     }
 
     public UserPoint use(final Long userId, final Long amount) {
+        long useAmount = useAmount(amount);
         UserPoint userPoint = userPointTable.selectById(userId);
-        return updatedPoint(userId, userPoint.point() - amount, TransactionType.USE);
+        return updatedPoint(userId, userPoint.point() - useAmount, TransactionType.USE);
+    }
+
+    public long useAmount(final long amount) {
+        if(amount < 0) {
+            throw new IllegalStateException("사용금액은 0원 이상이여야 합니다");
+        }
+        return amount;
     }
 
     public UserPoint select(final Long userId) {
         return userPointTable.selectById(userId);
     }
 
-    public UserPoint updatedPoint(final Long userId, final Long updatedPoint, TransactionType type) {
+    public UserPoint updatedPoint(final Long userId, final Long amount, TransactionType type) {
 
-        if (updatedPoint > 10_000_000L) {
-            throw new IllegalStateException("최대 10_000_000포인트까지 보유 가능합니다");
-        }
-        if (updatedPoint < 0) {
-            throw new IllegalStateException("잔고가 부족하여 사용이 불가능 합니다");
-        }
+        long updatedPoint = updatedPoint(amount);
 
         UserPoint updatedUserPoint = userPointTable.insertOrUpdate(userId, updatedPoint);
 
@@ -62,8 +66,21 @@ public class PointService {
         return updatedUserPoint;
     }
 
-    public List<PointHistory> getPointHistories(Long userId) {
-        return pointHistoryTable.selectAllByUserId(userId);
+    public long updatedPoint(final long updatedPoint) {
+        if (updatedPoint > 10_000_000L) {
+            throw new IllegalStateException("최대 10_000_000포인트까지 보유 가능합니다");
+        }
+        if (updatedPoint < 0) {
+            throw new IllegalStateException("잔고가 부족하여 사용이 불가능 합니다");
+        }
+        return updatedPoint;
     }
 
+    public List<PointHistory> getPointHistories(Long userId) {
+        List<PointHistory> histories = pointHistoryTable.selectAllByUserId(userId);
+        if (histories == null || histories.isEmpty()) {
+            return Collections.emptyList(); // null 대신 빈 리스트 반환
+        }
+        return histories;
+    }
 }
